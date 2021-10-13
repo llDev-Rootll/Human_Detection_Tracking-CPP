2971f61235d2c450f2a70611efd7b26715d9869f
aditiramadwar <aditiramadwar@hotmail.com>
2021-10-13 12:50:37 -0400

    Defined detectHumans method

--- src/Robot.cpp
+++ src/Robot.cpp
@@ -1,11 +1,15 @@
 #include "Robot.h"
 
-
 vector<cv::Mat> prepFrame(vector<cv::Mat> frame, vector<int> net_input_shape) {
 	vector<cv::Mat> dummy;
 	return dummy;
 }
+
 vector<double> transformToRobotFrame(Eigen::Matrix4f transformation_matrix, vector<Rect> bbox_coords, double pixel_to_meter, double area_to_depth) {
 	vector<double> dummy;
-	return 0
+	return dummy;
+}
+
+int detectHumans() {
+	return 0;
 }
