e14db4ebd4ab0654487a1fbad061d82b30f65575
aditiramadwar <aditiramadwar@hotmail.com>
2021-10-13 12:18:34 -0400

    Update HumanDetector methods to return dummy variables

--- src/HumanDetector.cpp
+++ src/HumanDetector.cpp
@@ -1,11 +1,16 @@
 #include "HumanDetector.h"
 
-
 vector<cv::Mat> HumanDetector::detection(cv::Net& net, cv::Mat& blob) {
-
+	vector<cv::Mat> dummy;
+	return dummy;
 }
 
 
 vector<cv::Rect> HumanDetector::postProcess(cv::Mat& frame, const vector<cv::Mat>& outs) {
+	vector<cv::Rect> dummy;
+	return dummy;
+}
 
+int HumanDetector:: drawBoundingBoxes(int classId, double confidence, int left, int top, int right, int bottom, cv::Mat& frame) {
+	return 0;
 }
