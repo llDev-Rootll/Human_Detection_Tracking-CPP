c3bd706a7a7e7d6edf45e190cd7be8c0f81e7156
aditiramadwar <aditiramadwar@hotmail.com>
2021-10-13 12:49:31 -0400

    Defined transformToRobotFrame method

--- src/Robot.cpp
+++ src/Robot.cpp
@@ -5,3 +5,7 @@ vector<cv::Mat> prepFrame(vector<cv::Mat> frame, vector<int> net_input_shape) {
 	vector<cv::Mat> dummy;
 	return dummy;
 }
+vector<double> transformToRobotFrame(Eigen::Matrix4f transformation_matrix, vector<Rect> bbox_coords, double pixel_to_meter, double area_to_depth) {
+	vector<double> dummy;
+	return 0
+}
