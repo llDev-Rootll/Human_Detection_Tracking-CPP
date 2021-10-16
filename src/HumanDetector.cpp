4f2feb197049dd085b55780a623bdc9f283891c2
aditiramadwar <aditiramadwar@hotmail.com>
2021-10-15 22:04:45 -0400

    Implementation of postProcess method, added bounding boxes

--- src/HumanDetector.cpp
+++ src/HumanDetector.cpp
@@ -65,12 +65,20 @@ int HumanDetector::postProcess(Mat& frame, const vector<Mat>& outs) {
 	// lower confidences
 	vector<int> indices;
 	cv::dnn::NMSBoxes(boxes, confidences, confidence_threshold, nms_threshold, indices);
+	// Draw bounding boxes and give labels
+	for (size_t i = 0; i < indices.size(); ++i) {
+		int idx = indices[i];
+		Rect box = boxes[idx];
+		drawBoundingBoxes(confidences[idx], box.x, box.y,
+				 box.x + box.width, box.y + box.height, frame, i);
+	}
 	/**
 	 * Later, it should return the dimensions and coordinated (box) of the bounding boxes
 	 * for transformation
 	 */
 	return 0;
 }
+
 /**
  * @brief 
  * 
@@ -84,6 +92,17 @@ int HumanDetector::postProcess(Mat& frame, const vector<Mat>& outs) {
  * @return int 
  */
 int HumanDetector::drawBoundingBoxes(double confidence, int left, int top, int right, int bottom, Mat& frame, int human_number) {
+	 //Draw a rectangle displaying the bounding box
+	cv::rectangle(frame, cv::Point(left, top), cv::Point(right, bottom), cv::Scalar(255, 178, 50), 3);
+	//Get the label for the class name and its confidence
+	string label = cv::format("%.2f", confidence);
+	label = "Human number : " +  std::to_string(human_number+1) + ", Confidence : "+label; 
+	//Display the label and confidence at the top of the bounding box
+	int baseLine;
+	Size labelSize = cv::getTextSize(label, cv::FONT_HERSHEY_SIMPLEX, 0.5, 1, &baseLine);
+	top = cv::max(top, labelSize.height);
+	cv::rectangle(frame, cv::Point(left, top - std::round(1.5*labelSize.height)), cv::Point(left + std::round(1.5*labelSize.width), top + baseLine), cv::Scalar(255, 255, 255), cv::FILLED);
+	cv::putText(frame, label, cv::Point(left, top), cv::FONT_HERSHEY_SIMPLEX, 0.75, cv::Scalar(0,0,0),1);
 	return 0;
 }
 
