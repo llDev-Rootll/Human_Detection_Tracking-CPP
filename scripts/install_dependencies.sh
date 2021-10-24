5d26c5cdf1742817f24c5ab7d90a5762342ee180
Arunava Basu <48803076+llDev-Rootll@users.noreply.github.com>
2021-10-23 20:16:43 -0400

    Updated install depencies

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -12,22 +12,22 @@ sudo apt-get install -y cmake git libgtk2.0-dev pkg-config libavcodec-dev libavf
 
 sudo apt-get install -y python-dev python-numpy libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libjasper-dev libdc1394-22-dev
 
-# wget -O opencv.zip https://github.com/opencv/opencv/archive/4.5.1.zip
-# wget -O opencv_contrib.zip https://github.com/opencv/opencv_contrib/archive/4.5.1.zip
-# unzip opencv.zip
-# unzip opencv_contrib.zip
+wget -O opencv.zip https://github.com/opencv/opencv/archive/4.5.1.zip
+wget -O opencv_contrib.zip https://github.com/opencv/opencv_contrib/archive/4.5.1.zip
+unzip opencv.zip
+unzip opencv_contrib.zip
 
-# cd opencv-4.5.1
-# mkdir build
-# cd build
+cd opencv-4.5.1
+mkdir build
+cd build
 
-# cmake -D CMAKE_BUILD_TYPE=RELEASE -D BUILD_EXAMPLES=OFF -D BUILD_opencv_apps=OFF -D BUILD_DOCS=OFF -D BUILD_PERF_TESTS=OFF -D BUILD_TESTS=OFF -D OPENCV_ENABLE_NONFREE=ON -D OPENCV_EXTRA_MODULES_PATH=../../opencv_contrib-4.5.1/modules -D CMAKE_INSTALL_PREFIX=/usr/local ..
+cmake -D CMAKE_BUILD_TYPE=RELEASE -D BUILD_EXAMPLES=OFF -D BUILD_opencv_apps=OFF -D BUILD_DOCS=OFF -D BUILD_PERF_TESTS=OFF -D BUILD_TESTS=OFF -D OPENCV_ENABLE_NONFREE=ON -D OPENCV_EXTRA_MODULES_PATH=../../opencv_contrib-4.5.1/modules -D CMAKE_INSTALL_PREFIX=/usr/local ..
 
-# make -j$(nproc)
+make -j$(nproc)
 
-# sudo make install
-sudo apt-get install libopencv-dev libopencv-contrib-dev
-# sudo sh -c 'echo "/usr/local/lib" > /etc/ld.so.conf.d/opencv.conf'
+sudo make install
+
+sudo sh -c 'echo "/usr/local/lib" > /etc/ld.so.conf.d/opencv.conf'
 sudo ldconfig
 cd ../../
 sudo rm -rf opencv-4.5.1
